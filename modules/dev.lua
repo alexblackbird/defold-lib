4925ca3010d0389259321a70e582ec45daff748c
Aleksandr Chizhevskiy <alexandr.gil@mail.ru>
2026-01-12 18:51:34 +0200

    up

--- modules/dev.lua
+++ modules/dev.lua
@@ -1,16 +1,25 @@
 local dev = {}
 
 function dev.init()
+	local sys_info = sys.get_sys_info()
 	if sys_info.system_name == "Windows" then
 		defos.set_window_size(40, 40, 900, 2000)
-		--sys_info.wallet_address = "0x08B4b984877eAaa728AcfF3Eb7063D186603e675"
 	end
 
 	if sys_info.system_name == "Darwin" then
-		--defos.set_window_size(70, 50, 480, 900)
-		--defos.set_window_size(70, 50, 350, 700)
 		defos.set_window_size(700, 100, 1080/2, 1920/2)
-		--sys_info.wallet_address = "0x08B4b984877eAaa728AcfF3Eb7063D186603e675"
+	end
+end
+
+function dev.on_input(self, action_id, action)
+	if action_id == hash("key_p") and action.released and sys.get_engine_info().is_debug then
+		if not self.profiler_flag then
+			profiler.enable_ui(true)
+			self.profiler_flag = true
+		else
+			profiler.enable_ui(false)
+			self.profiler_flag = false
+		end
 	end
 end
 
